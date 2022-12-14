cacf3dc93614f5c5b8cb91246942b6bc4e88f0f4
yangming <zhangym@dxy.cn>
2022-12-14 19:06:06 +0800

    feat: 调试

--- app/Http/Controllers/CounterController.php
+++ app/Http/Controllers/CounterController.php
@@ -27,6 +27,7 @@ class CounterController extends Controller
     {
         try {
             $head = $_SERVER;
+            
             return response()->json($head);
             $data = (new Counters)->find(1);
             if ($data == null) {
